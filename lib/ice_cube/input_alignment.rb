1ff96a5291ab3194275a922fa17d22981a3e3125
linkdigitalvietnam <dungtqa@gmail.com>
2025-05-12 15:21:09 +0700

    Update input_alignment.rb
    Fix bug

--- lib/ice_cube/input_alignment.rb
+++ lib/ice_cube/input_alignment.rb
@@ -8,16 +8,16 @@ module IceCube
 
     attr_reader :rule, :value, :rule_part
 
-    def verify(freq, options = {}, &)
+    def verify(freq, options = {})
       @rule.validations[:interval] or return
 
       case @rule
       when DailyRule
-        verify_wday_alignment(freq, &)
+        verify_wday_alignment(freq)
       when MonthlyRule
-        verify_month_alignment(freq, &)
+        verify_month_alignment(freq)
       else
-        verify_freq_alignment(freq, &)
+        verify_freq_alignment(freq)
       end
     end
 
